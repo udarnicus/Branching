fe90c6794ce31233ccd3d2eaef4884f1b64eff2c
Radu <udarnicus@gmail.com>
2020-05-26 18:04:36 +0200

    Feature2Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("asdfas");
 	    System.out.println("asdfas");
+        System.out.println("asdfas");
     }
 
     public void importBooks(){
